b740f3382d986d00352443fc8403ce954d3c4d65
Daniel Hahn <dha@betterplace.org>
2013-01-20 20:10:01 +0100

    Init w/ twitter bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TalkyTalky::Application.config.secret_token = '8294abf27c2521698533a006ea15a3eaf0aafaa490421133aa7140e83a5680f3a2317558a3d20501fe9a75b9b7a0da3fd0f1a4ff22eedd58de19d80b7eef8ce4'
