43745f3d4bbcb54ed559ae5602c3c220faeb25b3
Daniel Hahn <dha@betterplace.org>
2013-01-21 11:39:56 +0100

    More stuff for ruby

--- ruby_starter.rb
+++ ruby_starter.rb
@@ -4,5 +4,25 @@ def my_method(param_one) # Type of param_one is not fixed
 end
 
 def method2(an_object)
-  puts an_object.foobar
+  puts an_object.foobar # Call "foobar" on the object
 end
+
+class Fnord
+
+  def foobar
+    decide_foobar
+  end
+
+  def decide_foobar
+    if 1 != 2
+      "foobar"
+    else
+      "Hellolo"
+    end
+  end
+
+end
+
+# Example calls
+
+method2 Fnord.new
