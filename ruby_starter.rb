ef64f643f8177cdeb8b6b18c00b1d10248936236
Daniel Hahn <dha@betterplace.org>
2013-01-21 11:32:44 +0100

    Added example ruby file

---
+++ ruby_starter.rb
@@ -0,0 +1,8 @@
+def my_method(param_one) # Type of param_one is not fixed
+  puts "Hello"
+  puts param_one # It will put the result of param_one.to_s 
+end
+
+def method2(an_object)
+  puts an_object.foobar
+end
