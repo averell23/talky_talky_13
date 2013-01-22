754a5c44f90375287881bd8706dd3d05948f6647
Daniel Hahn <dha@betterplace.org>
2013-01-22 11:58:36 +0100

    Forms and creating talkies

--- app/controllers/talkies_controller.rb
+++ app/controllers/talkies_controller.rb
@@ -4,4 +4,14 @@ class TalkiesController < ApplicationController
     @talkies = Talkie.all
   end
 
+  def create
+    @talkie = Talkie.new(params[:talkie])
+    @talkie.save!
+    redirect_to talkies_path
+  end
+
+  def new
+    @talkie = Talkie.new
+  end
+
 end
