c24f6b9bb9f2ae7b2df14344b6d02a841ee9ab58
Daniel Hahn <dha@betterplace.org>
2013-01-22 14:23:06 +0100

    Creating talkies now nice

--- app/controllers/talkies_controller.rb
+++ app/controllers/talkies_controller.rb
@@ -1,13 +1,16 @@
 class TalkiesController < ApplicationController
 
   def index
-    @talkies = Talkie.all
+    @talkies = Talkie.order('id DESC').limit(20)
   end
 
   def create
     @talkie = Talkie.new(params[:talkie])
-    @talkie.save!
-    redirect_to talkies_path
+    if @talkie.save
+      redirect_to talkies_path
+    else
+      render :new
+    end
   end
 
   def new
--- app/models/talkie.rb
+++ app/models/talkie.rb
@@ -1,3 +1,6 @@
 class Talkie < ActiveRecord::Base
   attr_accessible :body
+
+  validates :body, length: { minimum: 10, maximum: 160 }
+  
 end
